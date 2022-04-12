236804567d15717058f150459d9ab0ca8835e4c8
Pedro Malaquias <psmalaquias1999@gmail.com>
2022-04-12 12:46:40 -0300

    Building the app and its routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ScrollContext } from "react-router-scroll-4";
 import Home from "./pages/Home";
+import UserList from "./components/users/UserList";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="teste" element={<div>teste</div>} />
+    <BrowserRouter basename={"/"}>
+      <ScrollContext>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/users`}
+            component={UserList}
+          />
           <Route path="*" element={<div>default</div>} />
-        </Route>
-      </Routes>
+        </Switch>
+      </ScrollContext>
     </BrowserRouter>
   );
 }
