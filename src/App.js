d0c3e1e52ffb398d04cac608e9bb167139374099
Pedro Barros <61165929+pbbdasilva@users.noreply.github.com>
2022-04-13 00:12:14 -0300

    add forms

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ScrollContext } from "react-router-scroll-4";
 import Home from "./pages/Home";
 import UserList from "./components/users/UserList";
-import SignUp from "./pages/signUp"
+import SignUp from "./pages/signUp";
+import OMForm from "./pages/OMForm";
+import DoencaForm from "./pages/DoencaForm";
 
 function App() {
   return (
@@ -19,6 +21,16 @@ function App() {
             exact
             path={`${process.env.PUBLIC_URL}/signup`}
             component={SignUp}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/admin-om`}
+            component={OMForm}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/admin-doenca`}
+            component={DoencaForm}
           />
           <Route path="*" element={<div>default</div>} />
         </Switch>
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,27 +1,32 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function Copyright(props: any) {
+function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -29,12 +34,12 @@ function Copyright(props: any) {
 const theme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
@@ -45,18 +50,23 @@ export default function SignUp() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-           Cadastrar usuário 
+            Cadastrar usuário
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -107,7 +117,7 @@ export default function SignUp() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-             Cadastrar 
+              Cadastrar
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
@@ -122,4 +132,3 @@ export default function SignUp() {
     </ThemeProvider>
   );
 }
-
