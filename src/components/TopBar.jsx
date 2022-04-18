8c43b1f851407799914aaf7d507276d1a92536e7
ajstourinho <ajstourinho@gmail.com>
2022-04-18 09:59:41 -0300

    Clean NavBar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,35 +1,17 @@
 import React from "react";
 import {Nav, Navbar, NavDropdown, Container} from 'react-bootstrap';
-import logo from "../images/logotipo.png";
+import Logotipo from "../images/logotipo.svg";
 
 function TopBar() {
   return (
     <Navbar bg="light" expand="lg" className="shadow-sm p-1">
       <Container>
         <Navbar.Brand href="/" className="d-flex align-items-center justify-content-center">
-          <img
-            src={logo}
-            alt="Logo"
-            height="50"
-            className="d-inline-block align-top"/>
+          <img src={Logotipo} alt="Logotipo IDQBRN" style={{height: "50px"}} />
           <h1 style={{fontSize: "25px", paddingTop: "5px"}}>&nbsp; &nbsp; IDQBRN &nbsp; &nbsp; </h1>
           <div style={{borderLeft: "1px solid black", height: "30px"}}></div>
           <h2 style={{fontSize: "23px", paddingTop: "5px"}}>&nbsp; &nbsp; Mapa Epidemiológico</h2>
         </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse> */}
       </Container>
     </Navbar>
   );
