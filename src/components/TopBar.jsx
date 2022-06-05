6899ce4da0840a72f0fadee7274c7b80d0417546
Pedro Malaquias <psmalaquias1999@gmail.com>
2022-06-05 18:02:58 -0300

    Improving the NavBar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../images/logotipo.png";
-import styles from './styles/TopBar.module.css';
+import styles from "./styles/TopBar.module.css";
 
 function TopBar() {
   return (
@@ -11,24 +11,32 @@ function TopBar() {
           href="/"
           className="d-flex align-items-center justify-content-center"
         >
+          <img
+            src={logo}
+            alt="Logo"
+            height="40"
+            className="d-inline-block align-top"
+          />
 
-        <img
-          src={logo}
-          alt="Logo"
-          height="50"
-          className="d-inline-block align-top"
-        />
+          <h1 className={styles.h1_title}>
+            &nbsp; &nbsp; IDQBRN &nbsp; &nbsp;{" "}
+          </h1>
 
-        <h1 className={styles.h1_title}>
-          &nbsp; &nbsp; IDQBRN &nbsp; &nbsp;{" "}
-        </h1>
-
-        <div className={styles.vertical_bar}></div>
-
-        <h2 className={styles.h2_subtitle}>
-          &nbsp; &nbsp; Mapa Epidemiológico
-        </h2>
+          <div className={styles.vertical_bar}></div>
         </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/" className="px-1">
+            <h3 className={styles.h3_options}>
+              &nbsp; &nbsp;Mapa Epidemiológico
+            </h3>
+          </Nav.Link>
+          <Nav.Link href="/users" className="px-1">
+            <h3 className={styles.h3_options}>&nbsp; &nbsp;Usuários</h3>
+          </Nav.Link>
+          <Nav.Link href="/desearses" className="px-1">
+            <h3 className={styles.h3_options}>&nbsp; &nbsp;Doenças</h3>
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
