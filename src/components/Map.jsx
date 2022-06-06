4e2e56794b4f4ff9c032a567f3fc6ab1ba7789ea
Alexandre Tourinho <49925174+ajstourinho@users.noreply.github.com>
2022-06-06 01:51:55 -0300

    Prepare Map for markers from Back

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,22 +9,27 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import '../styles/App.css';
 
 function Map() {
+
+  const markers = [[-10,-50], [-14,-54], [-5, -60]]
+
   return (
     <Card sx={{width: "55vw", height: "70vh", minWidth: 300, margin: 5 }} className='bg-light'>
 
     <div>
-      <MapContainer center={[-25, -28]} zoom={4} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <MapContainer center={[-25, -28]} zoom={4} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-            <Marker position={[-10, -40]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-      </MapContainer>
+            {markers.map((marker =>
+              <Marker position={marker}>
+                {/* <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup> */}
+              </Marker>
+            ))}
+        </MapContainer>
 
     </div>
 
